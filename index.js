5f8194d1def0574f7088dade0b086ca9eec2266a
Jeffery Walsh <cyberhorsey@gmail.com>
2023-01-04 17:43:51 -0800

    whatever

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import axios from "axios";
 // we use `import axios from "axios"` which is another way of saying `const axios = require("axios")`
 // it is jsut better supported in the browser!
+
+const button = document.getElementById("submit");
+
+button.addEventListener("click", function() {
+    const input = document.getElementById("whatever");
+    console.log(input.value);
+})
